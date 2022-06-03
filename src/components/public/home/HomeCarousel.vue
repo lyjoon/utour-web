1262f3394d6534bb5af26ce14c00c86c844cc8e6
lyjoon <lyjoon@gmail.com>
2022-06-04 05:08:06 +0900

    .

--- src/components/public/home/HomeCarousel.vue
+++ src/components/public/home/HomeCarousel.vue
@@ -1,41 +1,33 @@
 <template>
   <v-row dense :no-gutters="$vuetify.breakpoint.smAndDown">
     <v-col class="col-12" >
-      <v-carousel v-model="idx" :height="height"
+      <v-sheet color="grey lighten-3" rounded width="100%"
+               v-if="(carouselItems.length || 0) < 1"
+               :height="height" elevation="0" outlined class="justify-center d-flex align-center">
+        <v-icon size="90px" color="grey lighten-2">mdi-image</v-icon>
+      </v-sheet>
+      <v-carousel v-model="carouselItem" :height="height"
                   class="rounded align-center"
+                  v-if="(carouselItems.length || 0) > 0"
                   show-arrows-on-hover hide-delimiters>
         <v-carousel-item
-            v-for="item in items"
+            v-for="item in carouselItems"
             :key="item.id">
-          <template slot="default">
-            <!--
-            <v-parallax :src="item.src" dark height="100%" class="align-center">
-              <v-overlay absolute opacity="0.15">
-                <div class="text-center" style="opacity: 0.7">
-                  <v-card-title class="display-1 font-montserrat-300">제목인가</v-card-title>
-                  <v-chip :small="$vuetify.breakpoint.xsOnly"
-                          color="white"
-                          class="font-montserrat-300 text--secondary" link>
-                    <strong class="mt-2 mb-2">MORE</strong>
-                  </v-chip>
-                </div>
-              </v-overlay>
-            </v-parallax>
-            -->
-            <v-img :src="item.src" dark height="100%" class="align-end">
-              <v-overlay absolute opacity="0.15">
-                <div class="text-center">
-                  <v-card-title class="text-h6 text-md-h5 text-lg-h5 text-xl-h4 font-weight-bold pb-2 pb-md-4 pb-lg-4 pb-xl-4 pl-2 pr-2">이런</v-card-title>
-                  <v-chip :small="$vuetify.breakpoint.smAndDown"
-                          color="white"
-                          link
-                          text-color="white"
-                          outlined
-                          class="elevation-0 rounded caption" style="opacity: 0.69">더보기</v-chip>
-                </div>
-              </v-overlay>
-            </v-img>
-          </template>
+          <v-img :src="item.imageSrc" dark height="100%" class="align-end">
+            <v-overlay absolute opacity="0.15">
+              <div class="text-center">
+                <v-card-title class="text-h6 text-md-h5 text-lg-h5 text-xl-h4 font-weight-bold pb-2 pb-md-4 pb-lg-4 pb-xl-4 pl-2 pr-2" v-text="item.title" />
+                <v-chip :small="$vuetify.breakpoint.smAndDown"
+                        color="white"
+                        link
+                        text-color="white"
+                        outlined
+                        @click="redirect(item)"
+                        v-if="(item.linkUrl || '') != ''"
+                        class="elevation-0 rounded caption" style="opacity: 0.69">더보기</v-chip>
+              </div>
+            </v-overlay>
+          </v-img>
         </v-carousel-item>
       </v-carousel>
     </v-col>
@@ -43,6 +35,8 @@
 </template>
 
 <script>
+import displayApi from "@/api/DisplayApi";
+
 export default {
   computed: {
     height (){
@@ -65,14 +59,40 @@ export default {
       return v;
     },
   },
+  mounted() {
+    this.search()
+  },
   data: ()=>({
-    idx:null,
-    items: [
-      {id:0,
-        src:'https://blog.kakaocdn.net/dn/I9lzD/btq6UdLXBNH/akC3UQ9s6RiblD7a5jIKf1/img.jpg'
+    carouselItem:null,
+    carouselItems: [],
+  }),
+  methods:{
+    search: function(){
+      let dateTime = new Date().getTime();
+      displayApi.getCarousels().then(res => {
+        let data = res.data;
+        if(data && data.result && Array.isArray(data.result)) {
+          this.carouselItems = [];
+          // this.carouselItem = 0;
+          data.result.forEach(item => {
+            this.carouselItems.push({
+              carouselId: item.carouselId,
+              title: item.title,
+              imageSrc: `/api/v1/display/carousel/${item.carouselId}?t=${dateTime}`,
+              linkUrl: item.linkUrl,
+            });
+          })
+        }
+      })
+    },
+    redirect: function(item) {
+      if(item.linkUrl.indexOf("http:") > -1 || item.linkUrl.indexOf("https:") > -1){
+        window.open(item.linkUrl, '_blank');
+      } else {
+        window.open('http://' + item.linkUrl, '_blank');
       }
-    ],
-  })
+    }
+  }
 }
 </script>
 
--- src/components/public/home/HomeProduct.vue
+++ src/components/public/home/HomeProduct.vue
@@ -1,54 +1,49 @@
 <template>
   <div>
     <sub-title title="자주 찾는 상품" description="봉선생님께서 추천하는 여행상품" />
-    <v-row no-gutters dense>
-      <v-col class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4" v-for="item in dataList" v-bind:key="item.title">
-        <product-item :title="item.title" :description="item.desc" :src="item.src" />
+    <v-row no-gutters dense v-if="(commerces.length || 0) > 0">
+      <v-col class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4" v-for="(item, index) in commerces" v-bind:key="index">
+        <product-item :title="item.title" :description="item.description" :src="item.repImageSrc" />
       </v-col>
     </v-row>
+    <div class="pa-16 d-flex justify-center flex-fill body-2" v-if="(commerces.length || 0) < 1" v-text="'등록된 상품이 없습니다.'" />
   </div>
 </template>
 
 <script>
 import ProductItem from "../product/list/ProductListItem";
 import SubTitle from "../../common/SubTitle";
+import displayApi from "@/api/DisplayApi";
 export default {
   name: "HomeProduct",
   components: {SubTitle, ProductItem},
   data : ()=>({
-    dataList : [
-      {
-        title: '로빈슨클럽(CLUB)',
-        desc: '카오락',
-        src: 'http://www.honeymoonlove.co.kr/resort_images/x_khao-lak-resort-pool10.jpg',
-      },
-      {
-        title: '노쿠 아일랜드',
-        desc: '수상비행기',
-        src: 'http://www.honeymoonlove.co.kr/resort_images/NM_Aerial-07.jpg'
-      },
-      {
-        title: '인아야 푸트리 발리',
-        desc: '발리',
-        src: 'https://t4.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/2UUt/image/-qXunTs8JxA83Ec1FaCN-L7aMKY'
-      },
-      {
-        title: '디스커버리 까르띠까 플라자 호텔',
-        desc: '발리',
-        src: 'http://www.honeymoonlove.co.kr/resort_images/4_14.jpg'
-      },
-      {
-        title: '유앤미 바이 코쿤',
-        desc: '수상비행기',
-        src: 'http://www.honeymoonlove.co.kr/resort_images/PV%20-%202%20EX.jpg'
-      },
-      {
-        title: '그랜드파크 로얄',
-        desc: '로스카보스',
-        src: 'http://www.honeymoonlove.co.kr/resort_images/_MG_0612.jpg'
-      }
-    ]
-  })
+    commerces:[]
+  }),
+  mounted() {
+    this.search();
+  },
+  methods:{
+    search: function(){
+      let dateTime = new Date().getTime();
+      displayApi.getCommerces().then(res=>{
+        let data = res.data;
+        if(data && data.result && Array.isArray(data.result)) {
+          this.commerces = [];
+          data.result.forEach(item => {
+            this.commerces.push({
+              commerceId : item.commerceId,
+              productId: item.productId,
+              title: item.title,
+              description : item.description,
+              repImageSrc: `/api/v1/product/image/${item.productId}?t=${dateTime}`
+            });
+          });
+        }
+
+      })
+    }
+  }
 }
 </script>
 
